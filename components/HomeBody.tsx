b9e50b7b6b1a43b3e6bcc1ee1090fb1e9720e82f
Lucas Lawrence <47094651+lucasklawrence@users.noreply.github.com>
2025-06-22 10:40:14 -0700

    update ismobile in home body

--- components/HomeBody.tsx
+++ components/HomeBody.tsx
@@ -1,24 +1,21 @@
 'use client'
 
 import { CourtSvg } from './court/CourtSvg'
-import { useRouter } from 'next/navigation'
 import { CourtContainer } from './court/CourtContainer'
 import React, { useRef, useState } from 'react'
 import { useHasSeenTour } from '@/utils/useHasSeenTour'
 import { CourtInteractionLayer } from './court/CourtInteractionLayer'
-import { useWindowSize } from '@/utils/useWindowSize'
 import { tourSteps } from '@/constants/tourSteps'
 import { FreeRoamOverlay } from './FreeRoamOverlay'
 import { MobileAdvanceOverlay } from './MobileAdvanceOverlay'
 import { TutorialOverlay } from './TutorialOverlay'
 import { useTourState } from './hooks/useTourState'
 import { useZoneContent } from './hooks/useZoneContent'
+import { useIsMobile } from '@/utils/hooks/useIsMobile'
 
 export function HomeBody() {
-  const router = useRouter()
   const { hasSeen, markAsSeen, reset } = useHasSeenTour()
-  const { width } = useWindowSize()
-  const isMobile = width <= 768
+  const isMobile = useIsMobile();
   const svgRef = useRef<SVGSVGElement>(null)
 
   const {
