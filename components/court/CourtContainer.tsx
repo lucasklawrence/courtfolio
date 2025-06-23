2061a4e65f0c1eb08aba0496fb4f4e17cb2f599a
Lucas Lawrence <47094651+lucasklawrence@users.noreply.github.com>
2025-06-22 17:18:54 -0700

    landscape bug and globals update

--- components/court/CourtContainer.tsx
+++ components/court/CourtContainer.tsx
@@ -9,7 +9,7 @@ import React from 'react'
 export const CourtContainer: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="fixed inset-0 bg-neutral-900 flex items-center justify-center touch-pan-x touch-pan-y">
-      <div className="relative w-[min(100vw,calc(100vh*1.5))] h-[min(100vh,calc(100vw/1.5))]">
+<div className="relative w-[min(100vw,calc(100svh*1.5))] h-[min(100svh,calc(100vw/1.5))]">
         {children}
       </div>
     </div>
