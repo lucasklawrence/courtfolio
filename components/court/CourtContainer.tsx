776950568b909f9d4a7edd1a12accf7e746d642b
Lucas Lawrence <47094651+lucasklawrence@users.noreply.github.com>
2025-06-22 17:23:52 -0700

    new fixes

--- components/court/CourtContainer.tsx
+++ components/court/CourtContainer.tsx
@@ -8,8 +8,8 @@ import React from 'react'
  */
 export const CourtContainer: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="fixed inset-0 bg-neutral-900 flex items-center justify-center touch-pan-x touch-pan-y">
-<div className="relative w-[min(100vw,calc(100svh*1.5))] h-[min(100svh,calc(100vw/1.5))]">
+    <div className="absolute inset-0 bg-neutral-900 flex items-center justify-center touch-pan-x touch-pan-y">
+      <div className="relative w-[min(100vw,calc(100svh*1.5))] h-[min(100svh,calc(100vw/1.5))]">
         {children}
       </div>
     </div>
